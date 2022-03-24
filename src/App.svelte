bc20af79d4da03c72d77a6434b76bd23dbaaa34d
Salman Usman <salmanusmank7@gmail.com>
2022-03-24 21:43:51 +0500

    reactive-assignment

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,17 @@
 <script>
-  let count = 1
+  let number = 0
 
-  function increment (){
-    count++
+  function add5(){
+    number += 5;
   }
+  
+  $: multiple = number*2 
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen space-y-4">
-  {count}
-  <button on:click="{increment}" class="btn btn-primary">Button</button>
-
+  <p> The number is: {number}</p>
+  <button on:click="{add5}" class="btn btn-primary">Add five</button>
+  <p>The number multiplied by 2 is {multiple}</p>
 </main>
 
 <style>
